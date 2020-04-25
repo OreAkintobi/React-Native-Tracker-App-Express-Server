dcdda12760ab153e71b705df5f314d446837cae3
OreAkintobi <oreakintobi@gmail.com>
2020-04-25 19:10:54 +0100

    set up mongo db with mongoose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (reques, response) => {
-  response.send("Hi, there!");
+const mongoUri =
+  "mongodb+srv://admin:adminpassword@cluster0-lkrfn.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error("Error connecting to mongo", error);
+});
+
+app.get("/", (request, response) => {
+  response.send("Hi, there! This server is built with mongoDB and Express");
 });
 
 app.listen(3000, () => {
