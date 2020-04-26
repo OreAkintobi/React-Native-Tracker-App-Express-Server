1ba7b82fe20ccf87d0544647c968da0c0f831a43
OreAkintobi <oreakintobi@gmail.com>
2020-04-26 20:43:07 +0100

    added get and post routes for track

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
+
 const User = mongoose.model("User");
 const SECRET_KEY = "MY SECRET KEY";
 
