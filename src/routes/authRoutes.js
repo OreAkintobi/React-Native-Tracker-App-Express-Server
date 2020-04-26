6320a6b39dd8e2cbdddb36e08ad60d00a0d55d8a
OreAkintobi <oreakintobi@gmail.com>
2020-04-26 18:34:22 +0100

    added new user post method

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
-  console.log(req.body);
-  res.send("You made a post request");
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = new User({ email, password });
+  await user.save();
+
+  res.send("You have just made a post request");
 });
 
 module.exports = router;
